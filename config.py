2ebb42302a5883ce0cb6f8d3e75ddf91898119c7
Rohan Gupta <75713762+torohangupta@users.noreply.github.com>
2025-11-08 11:14:56 -0600

    undo some of my changes

--- config.py
+++ config.py
@@ -3,12 +3,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-token = os.environ.get("token")
-game_number = os.environ.get("game_number")
-gamestate_path = os.environ.get("gamestate_path")
 
+token = os.environ.get("token")
+
+
+with open("config.json", "r") as f:
+    data = json.load(f)
+    game_number = data["game_number"]
+    gamestate_path = data["gamestate_path"]
+    f.close()
 
 def update_game_number():
     with open("config.json", "w") as f:
-        game_number += 1
-        json.dump(game_number, f)
+        data["game_number"] += 1
+        json.dump(data, f)
